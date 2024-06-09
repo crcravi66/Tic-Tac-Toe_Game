329c36c900de6abba15783bdb84ef4f4b8c86e69
arun <crcravi66@gmail.com>
2024-06-09 12:57:24 +0530

    add for winning combiation file

--- Tic-Tac-Toe-Game/src/App.jsx
+++ Tic-Tac-Toe-Game/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import Log from './components/Log'
 import Player from "./components/Player"
 import GameBoard from "./components/GameBoard"
-
+import { WINNING_COMBINATIONS } from './winninng-combination';
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = 'X';
